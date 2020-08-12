39b4539e2b1cb9db60f1717b1848e03386e742ff
NBanski <n.d.banski@gmail.com>
2020-08-12 15:45:34 +0200

    api key handling done

--- api.py
+++ api.py
@@ -3,9 +3,24 @@ path = os.getcwd() + "\env\\api.env"
 
 def load_api():
     try:
-        i = open(path, 'r').readline().split('=')[1].strip("'")
-        return i
-    finally:
-        print("check")
+        with open(path, "r") as f:
+            key = f.readline().split('=')[1].strip('"')
+        return key
+    except FileNotFoundError:
+        print("File not found! Creating files...")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w") as f:
+            f.write("")
+        print("Done.")
+        set_api()
+    except IndexError:
+        print("No valid API Key was found.")
+        set_api()
 
-load_api()
+def set_api():
+    print("Enter valid VirusTotal API KEY:")
+    key = str(input("> "))
+    with open(path, "r+") as f:
+        f.write("KEY=" + '"' + key + '"')
+
+API_KEY = load_api()
--- endpoints.py
+++ endpoints.py
@@ -1,22 +1,22 @@
 import requests
 import hashlib
-from api import API_PUBLIC
+from api import API_KEY
 
 m_url = 'https://www.google.com'
 
 def make_scan_request(m_url):
     url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-    params = {'apikey': API_PUBLIC, 'url': m_url}
+    params = {'apikey': API_KEY, 'url': m_url}
     response = requests.post(url, data=params)
     print(response.json())
 
 def make_report_request(id):
     url = 'https://www.virustotal.com/vtapi/v2/url/report'
-    params = {'apikey': API_PUBLIC, 'resource' : id}
+    params = {'apikey': API_KEY, 'resource' : id}
     response = requests.post(url, params)
     print(response.json())
 
 # make_request(prep_url)
 # make_report_request('d0e196a0c25d35dd0a84593cbae0f38333aa58529936444ea26453eab28dfc86-1597233114')
 
-print(API_PUBLIC)
+print(API_KEY)
