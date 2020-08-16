66d0b24cfeacaa5640b5090f917247d397dc526d
NBanski <n.d.banski@gmail.com>
2020-08-16 22:32:59 +0200

    dirs

--- log.py
+++ log.py
@@ -1,11 +1,13 @@
 import os
-log_url_scan = os.getcwd() + "\\logs\\url scan\\"
-log_url_report = os.getcwd() + "\\logs\\url report\\"
+log_url_scan = os.path.join(os.getcwd(), 'logs', 'url scan')
+log_url_report = os.path.join(os.getcwd(), 'logs', 'url report')
 
 # Creates directory for logs.
 def create_log_dir():
     try:
-        os.mkdir(log_url_scan)
-        os.mkdir(log_url_report)
+        os.makedirs(log_url_scan)
+        os.makedirs(log_url_report)
     except FileExistsError:
         print("There's already one one more logs directories.")
+
+create_log_dir()
